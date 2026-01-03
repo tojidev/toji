f6dbffe582937f67c1028275e47d7e0ed25be5e0
Prince <prince@Princes-MacBook-Pro.local>
2026-01-03 12:23:49 +0530

    color changed

--- src/app/(auth)/admin-components/Sidebar.tsx
+++ src/app/(auth)/admin-components/Sidebar.tsx
@@ -4,16 +4,10 @@ import Link from "next/link";
 const Sidebar = () => {
   return (
     <>
-      <aside className="fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-99999 border-r border-gray-200 lg:w-[290px] -translate-x-full lg:translate-x-0">
+      <aside className="fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white text-gray-900 h-screen transition-all duration-300 ease-in-out z-99999 border-r border-gray-200 lg:w-[290px] -translate-x-full lg:translate-x-0">
         <div className="py-8 flex justify-start">
           <Link href="/">
-            <img
-              className="dark:hidden"
-              src="/images/logo.png"
-              alt="Logo"
-              width="70"
-              height="40"
-            />
+            <img src="/images/logo.png" alt="Logo" width="70" height="40" />
           </Link>
         </div>
 
