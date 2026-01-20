b46e9e7cdd30271e6dff73da9e76fcb8bf229538
Prince <prince@Princes-MacBook-Pro.local>
2026-01-20 14:11:02 +0530

    Checking this for testing purpose

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -7,8 +7,20 @@ import { getClients } from "@/helper/works";
 export default async function Home() {
   const clientsData = await getClients();
 
+  const fetchData = await fetch(`${process.env.API_BASE_URL}/works`, {
+    cache: "no-cache",
+  });
+  const clientFetch = await fetchData.json();
+
+  console.log(clientFetch);
+
   return (
     <>
+      <ContentItemContainer
+        clients={clientFetch}
+        pageName="Work"
+        pageLink="/work/"
+      />
       <PageContainer
         LeftContents={<LeftStaticContent />}
         RightcontentItems={
